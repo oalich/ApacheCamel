e52063067021f18bab332d9c44ce3227242fd80f
oalich <oalich@gmail.com>
2022-11-15 09:28:06 +0000

    SpringBoot Initializr

---
+++ src/main/java/de/alich/cameltest/CameltestApplication.java
@@ -0,0 +1,13 @@
+package de.alich.cameltest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CameltestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CameltestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/alich/cameltest/CameltestApplicationTests.java
@@ -0,0 +1,13 @@
+package de.alich.cameltest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CameltestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
